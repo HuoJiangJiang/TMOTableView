59f3372b64acdbc54a6d83f5d839850ef1950da0
ponycui <PonyCui_Home@saitekiiMac.local>
2014-07-13 12:41:27 +0800

    Add TMOEmptyDataSetDemo

---
+++ TMOEmptyDataSetDemo/TMOEmptyDataSetDemo/DemoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DemoAppDelegate.h
+//  TMOEmptyDataSetDemo
+//
+//  Created by 崔 明辉 on 14-7-13.
+//  Copyright (c) 2014年 崔 明辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DemoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TMOEmptyDataSetDemo/TMOEmptyDataSetDemoTests/TMOEmptyDataSetDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  TMOEmptyDataSetDemoTests.m
+//  TMOEmptyDataSetDemoTests
+//
+//  Created by 崔 明辉 on 14-7-13.
+//  Copyright (c) 2014年 崔 明辉. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TMOEmptyDataSetDemoTests : XCTestCase
+
+@end
+
+@implementation TMOEmptyDataSetDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
